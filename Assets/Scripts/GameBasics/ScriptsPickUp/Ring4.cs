4b4f438611d6c2f5386abfb89bd3d8782e9db02d
madelino22 <madelino22@ucm.es>
2020-04-20 13:57:37 +0200

    Organizando carpetas de script y scenes y arreglando algunos fallos

---
+++ Assets/Scripts/GameBasics/ScriptsPickUp/Ring4.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring4 : MonoBehaviour
+{
+    private bool recogible;
+    private GameObject player;
+
+
+    void Start()
+    {
+        if (recogible && Input.GetKey(KeyCode.E)) PickUp();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Movimiento8D>() != null)
+        {
+            player = other.gameObject;
+            recogible = true;
+        }
+    }
+
+    void OnTriggerExit2D()
+    {
+        recogible = false;
+    }
+    void PickUp()
+    {
+        //player.GetComponent<AnilloCuatro>().enabled = true;
+    }
+}
