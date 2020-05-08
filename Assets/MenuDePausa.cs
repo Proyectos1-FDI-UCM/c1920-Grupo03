e4d8eab3b7b6685d880d531114af192ef6ad3399
madelino22 <madelino22@ucm.es>
2020-05-08 19:30:06 +0200

    Menu de pausa terminado

---
+++ Assets/MenuDePausa.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuDePausa : MonoBehaviour
+{
+
+
+    public static bool isPaused;
+    public GameObject pauseMenu;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else Pause();
+        }
+
+
+       
+    }
+    public void Resume()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+
+    void Pause()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
