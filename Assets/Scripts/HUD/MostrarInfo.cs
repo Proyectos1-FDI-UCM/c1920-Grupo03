17f02037e7d91c769f612261e5591978a8a76258
AlexSopena <62748649+AlexSopena@users.noreply.github.com>
2020-05-12 10:55:32 +0200

    Añadidos mensajes de información al acercarse a las armas.

---
+++ Assets/Scripts/HUD/MostrarInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MostrarInfo : MonoBehaviour
+{
+    [SerializeField]
+    SpriteRenderer UIelement;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        UIelement.enabled = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        UIelement.enabled = false;
+    }
+}
