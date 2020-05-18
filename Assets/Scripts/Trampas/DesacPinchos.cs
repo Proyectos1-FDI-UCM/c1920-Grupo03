64527dbc071b24845cbf405634a2b77881699f29
Jonandra <63009653+Jonandra@users.noreply.github.com>
2020-05-18 20:05:46 +0200

    Corrección fallo con trampas de pinchos

--- Assets/Scripts/Trampas/DesacPinchos.cs
+++ Assets/Scripts/Trampas/DesacPinchos.cs
@@ -7,6 +7,10 @@ public class DesacPinchos : MonoBehaviour
     
     public void Desactivar()
     {
-        for (int x = 0; x < transform.childCount; x++) transform.GetChild(x).GetComponent<TrampaPinchos>().enabled = false;
+        for (int x = 0; x < transform.childCount; x++)
+        {
+            transform.GetChild(x).GetComponent<TrampaPinchos>().enabled = false;
+            Destroy(transform.GetChild(x).GetComponent<Collider2D>());
+        }
     }
 }
