7486d8449c9385f620b922bff7ad3158f4c81ad4
AlexSopena <62748649+AlexSopena@users.noreply.github.com>
2020-05-18 21:13:01 +0200

    Añadidos sprites para casi todos los objetos

--- Assets/Scripts/Trampas/TrampaPinchos.cs
+++ Assets/Scripts/Trampas/TrampaPinchos.cs
@@ -4,43 +4,29 @@ using UnityEngine;
 
 public class TrampaPinchos : MonoBehaviour
 {
-    private Renderer cambiaColor;
     private bool pinchando;
+
     void Start()
     {
-        cambiaColor = GetComponent<Renderer>();
         pinchando = false;
     }
+
     void OnTriggerEnter2D()
     {
         if (!pinchando)
         {
             pinchando = true;
-            Invoke("Advertencia", 0f);
             Invoke("Ataque", 3f);
-            Invoke("ColorInicial", 3.5f);
 
         }
-
-
     }
     void OnTriggerExit2D()
     {
         pinchando = false;
     }
-    void ColorInicial()
-    {
-        cambiaColor.material.color = Color.black;
-    }
-
-    void Advertencia()
-    {
-        cambiaColor.material.color = Color.blue;
-    }
-
+    
     void Ataque()
     {
-        cambiaColor.material.color = Color.magenta;
         if (pinchando)
         {
             GameManager.instance.TakeDamage(15);
