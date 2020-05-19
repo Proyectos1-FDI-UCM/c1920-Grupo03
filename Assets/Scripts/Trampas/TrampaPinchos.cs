15fd8346a56ee682237b0cb87931ce9b546b2855
Jonandra <63009653+Jonandra@users.noreply.github.com>
2020-05-19 18:27:29 +0200

    animacion trampa arreglada

--- Assets/Scripts/Trampas/TrampaPinchos.cs
+++ Assets/Scripts/Trampas/TrampaPinchos.cs
@@ -5,33 +5,42 @@ using UnityEngine;
 public class TrampaPinchos : MonoBehaviour
 {
     private bool pinchando;
+    Animator anim;
+    float hit = 0, activateRate= 2;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
         pinchando = false;
     }
 
-    void OnTriggerEnter2D()
+    void OnTriggerEnter2D(Collider2D col)
     {
-        if (!pinchando)
-        {
-            pinchando = true;
-            Invoke("Ataque", 3f);
+        if (col.GetComponent<Movimiento8D>()) { 
+            if (!pinchando && Time.time > hit)
+            {
+                activateRate = Time.time + hit;
+                anim.SetBool("actvidado", true);
+                pinchando = true;
+                Invoke("Ataque", 1.5f);
 
-        }
+            }
+    }
     }
     void OnTriggerExit2D()
     {
+        //anim.SetBool("actvidado", false);
         pinchando = false;
     }
     
     void Ataque()
     {
+        
         if (pinchando)
         {
             GameManager.instance.TakeDamage(15);
             pinchando = false;
         }
-        
+        anim.SetBool("actvidado", false);
     }
 }
