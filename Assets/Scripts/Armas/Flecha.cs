c878841cf49bd3e9b4081f3a06d83c9f61be626d
AlexSopena <62748649+AlexSopena@users.noreply.github.com>
2020-05-26 20:16:48 +0200

    Mejorado script flecha

--- Assets/Scripts/Armas/Flecha.cs
+++ Assets/Scripts/Armas/Flecha.cs
@@ -4,39 +4,18 @@ using UnityEngine;
 
 public class Flecha : MonoBehaviour
 {
-    public float velocidad;
-    public GameObject player;
-    private Vector2 objetivo;
-    private Rigidbody2D rb;
     EnemyHealth enemyHealth;
     public int daño = 50;
-    
+
 
     private void OnCollisionEnter2D(Collision2D collision)//La flecha se destruye con  el mapa,
     {
-
         enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
-        if ( collision.gameObject.tag == "Mapa")
-        {
-            DestruirFlecha();
-        }
-        else if(collision.gameObject.tag == "Player")
-        {
-            DestruirFlecha();
-            
-        }
-        else if (enemyHealth != null)
-        {
+
+        if (enemyHealth != null)
             enemyHealth.TakeDamage(daño);
-            DestruirFlecha();
-        }
-        else if(collision.gameObject.layer == 16 || collision.gameObject.layer == 13)
-        {
-            DestruirFlecha();
-        }
-    }
-    private void DestruirFlecha()
-    {
+
         Destroy(gameObject);
     }
+        
 }
