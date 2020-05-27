12a39ee1da4561bc5d6a4c9590b4aaf76597f96f
madelino22 <madelino22@ucm.es>
2020-05-27 10:37:15 +0200

    Arreglada ballesta

--- Assets/Scripts/Trampas/Ballesta.cs
+++ Assets/Scripts/Trampas/Ballesta.cs
@@ -10,7 +10,7 @@ public class Ballesta : MonoBehaviour
     private GameObject arrow;
     public float fireRate;
     private float nextFire;
-
+    [SerializeField] float velFlechas;
 
     Vector2 dirFlecha;
     private void Start()
@@ -22,11 +22,17 @@ public class Ballesta : MonoBehaviour
         if (Time.time > nextFire)
         {
             dirFlecha = new Vector2(shotSpawn.position.x - transform.position.x, shotSpawn.position.y - transform.position.y);
+            dirFlecha.Normalize();
             nextFire = Time.time + fireRate;
             Vector3 pos = new Vector3(shot.transform.position.x, shot.transform.position.y, 0);
             arrow = Instantiate<GameObject>(shot, shotSpawn.position, Quaternion.identity);
             Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
-            rb.velocity = dirFlecha;
+
+            rb.velocity = dirFlecha * Time.deltaTime * velFlechas*100 ;
+
+            Vector3 orientacion = new Vector3(0, 0, this.transform.rotation.eulerAngles.z + 90);//para que no sea en quaterniones hay que poner euelrangles
+            arrow.transform.Rotate(orientacion);
+            
 
 
 
