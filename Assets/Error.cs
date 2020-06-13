f2f8e5e6af5fb71ab54ae2b5dc267980c564d466
madelino22 <madelino22@ucm.es>
2020-06-13 16:46:28 +0200

    Build final

---
+++ Assets/Error.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Error : MonoBehaviour
+{
+    private void OnDisable()
+    {
+        Debug.LogError("Borrado");
+    }
+}
